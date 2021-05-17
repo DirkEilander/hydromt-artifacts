a7d5b4e86f94c04221fb3ab113be339eef2f8e41
DirkEilander <dirk.eilander@gmail.com>
2021-05-17 23:02:42 +0200

    fix tar

--- create_artifacts.py
+++ create_artifacts.py
@@ -10,7 +10,7 @@ import tarfile
 
 def make_tarfile(output_filename, source_dir):
     with tarfile.open(output_filename, "w:gz") as tar:
-        tar.add(source_dir)
+        tar.add(source_dir, arcname=".")
 
 
 #%% versions and new data
